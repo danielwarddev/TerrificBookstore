fd8fd1cc99a24f4e3a48598c94ab6432f2e670f0
Daniel Ward <dward256@hotmail.com>
2024-01-18 11:46:25 -0600

    added service builder

--- TerrificBookstore/Program.cs
+++ TerrificBookstore/Program.cs
@@ -1,15 +1,22 @@
+using TerrificBookstore.API.Books;
+using TerrificBookstore.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpClient<IBookClient, BookClient>(client =>
+{
+    client.BaseAddress = new Uri("https://gutendex.com/");
+    client.Timeout = TimeSpan.FromSeconds(30);
+});
+
+builder.Services.AddDbContext<BookstoreContext>(options => options.UseNpgsql("Host=localhost;Database=bookstore;Username=postgres;Password=postgres"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,3 +30,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
